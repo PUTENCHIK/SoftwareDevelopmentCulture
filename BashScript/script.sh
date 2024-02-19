16bec7268c98479c072075c0a6105b9d05720585
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-02-19 13:30:47 +0000

    Modified script.sh for task.

--- BashScript/script.sh
+++ BashScript/script.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-url="http://158.160.110.45"
+url="158.160.110.45"
 request="/user/register"
+json='{"nickname":"'$1'", "password":"'$2'"}'
 
-response$(curl -Is "$url" | head -n 1)
+response=$(curl -s -X POST -H "Content-Type: application/json" -d "$json" "http://$url$request" | head -n 1)
 
-if [[ "$response" == *"200 OK"* ]]; then
-	echo "IP is okay."
-else 
-	echo "There isn't answer."
-fi
+echo $response
